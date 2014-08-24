5658fdc14041b0647e5c032cb362f270ac6b0158
sanjiv sahayam <sanjsmailbox@gmail.com>
2014-08-24 17:22:38 +1000

    1. fixed definition of skips []. 2. Use function composition to simplify filterSkips. 3. Added a property test that verifies that the skiped length == the unskipped length

--- tests/Chapter3/Tests.hs
+++ tests/Chapter3/Tests.hs
@@ -1,11 +1,23 @@
-module Chapter3.Tests (skips1, skips2, skips3, skips4, skips5) where
+module Chapter3.Tests (unitTests, propertyTests) where
 
 import Chapter3.Golf
+import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+
+unitTests ::  TestTree
+unitTests =  testGroup "unit tests for skip" [
+                testCase "emptyList" skips1,
+                testCase "singletonList" skips2,
+                testCase "listWithFourCharacters" skips3,
+                testCase "listWithSixCharacters" skips4,
+                testCase "listTrueAndFalse" skips5
+            ]
 
 
 skips1 :: Assertion
-skips1 = skips ([]::[Int]) @?= ([[]] :: [[Int]])
+
+skips1 = skips ([]::[Int]) @?= ([] :: [[Int]])
 
 skips2 :: Assertion
 skips2 =  skips ([1] :: [Int]) @?= ([[1]] :: [[Int]])
@@ -18,3 +30,8 @@ skips4 =  skips "hello!" @?= ["hello!", "el!", "l!", "l", "o", "!"]
 
 skips5 :: Assertion
 skips5 =  skips [True, False] @?= [[True, False], [False]]
+
+propertyTests :: TestTree
+propertyTests = testGroup "(checked by QuickCheck)"
+  [ QC.testProperty "length xs == length (skips xs)" $
+      \xs -> length (xs :: [Int]) == length (skips xs)]
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Test.Tasty
-import Test.Tasty.HUnit
 import qualified Chapter3.Tests as Chap3
 
 main :: IO ()
@@ -9,9 +8,5 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" [
-            testCase "emptyList" Chap3.skips1,
-            testCase "singletonList" Chap3.skips2,
-            testCase "listWithFourCharacters" Chap3.skips3,
-            testCase "listWithSixCharacters" Chap3.skips4,
-            testCase "listTrueAndFalse" Chap3.skips5
+            Chap3.unitTests, Chap3.propertyTests
         ]
